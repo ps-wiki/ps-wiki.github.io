b4f8dbb7fe54e40b4b1586487fc1ee88436a56de
leocuisine <280889505+leocuisine@users.noreply.github.com>
2026-09-09 23:33:26 -0700

    perf: defer optional math and table scripts

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -17,9 +17,40 @@ window.MathJax = {
   },
 };
 
-document$.subscribe(() => {
-  MathJax.startup.output.clearCache();
-  MathJax.typesetClear();
-  MathJax.texReset();
-  MathJax.typesetPromise();
-});
+let mathJaxLoadPromise;
+
+function loadMathJax() {
+  if (window.MathJax.typesetPromise) {
+    return Promise.resolve(window.MathJax);
+  }
+
+  if (!mathJaxLoadPromise) {
+    mathJaxLoadPromise = new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = "https://unpkg.com/mathjax@3/es5/tex-mml-chtml.js";
+      script.async = true;
+      script.onload = () => resolve(window.MathJax);
+      script.onerror = reject;
+      document.head.appendChild(script);
+    });
+  }
+
+  return mathJaxLoadPromise;
+}
+
+function typesetMath() {
+  if (!document.querySelector(".arithmatex")) {
+    return;
+  }
+
+  loadMathJax()
+    .then(() => {
+      window.MathJax.startup.output.clearCache();
+      window.MathJax.typesetClear();
+      window.MathJax.texReset();
+      return window.MathJax.typesetPromise();
+    })
+    .catch((error) => console.error("MathJax failed to load", error));
+}
+
+document$.subscribe(typesetMath);
--- docs/javascripts/tablesort.js
+++ docs/javascripts/tablesort.js
@@ -1,6 +1,39 @@
-document$.subscribe(function () {
-  var tables = document.querySelectorAll("article table:not([class])");
-  tables.forEach(function (table) {
-    new Tablesort(table);
-  });
-});
+let tablesortLoadPromise;
+
+function loadTablesort() {
+  if (window.Tablesort) {
+    return Promise.resolve(window.Tablesort);
+  }
+
+  if (!tablesortLoadPromise) {
+    tablesortLoadPromise = new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = "https://unpkg.com/tablesort@5.3.0/dist/tablesort.min.js";
+      script.async = true;
+      script.onload = () => resolve(window.Tablesort);
+      script.onerror = reject;
+      document.head.appendChild(script);
+    });
+  }
+
+  return tablesortLoadPromise;
+}
+
+function initTablesort() {
+  const tables = Array.from(document.querySelectorAll("article table:not([class])")).filter((table) => !table.dataset.tablesortInitialized);
+
+  if (!tables.length) {
+    return;
+  }
+
+  loadTablesort()
+    .then(() => {
+      tables.forEach((table) => {
+        new window.Tablesort(table);
+        table.dataset.tablesortInitialized = "true";
+      });
+    })
+    .catch((error) => console.error("Tablesort failed to load", error));
+}
+
+document$.subscribe(initTablesort);
